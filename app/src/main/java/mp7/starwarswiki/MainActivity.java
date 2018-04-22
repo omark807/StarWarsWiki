423c9cd1fa980517a7cd9f419cd7a4bb7d916715
OmarK807 <omark807@gmail.com>
2018-04-21 21:29:16 -0500

    Figured out how buttons work.

--- app/src/main/java/mp7/starwarswiki/MainActivity.java
+++ app/src/main/java/mp7/starwarswiki/MainActivity.java
@@ -1,13 +1,47 @@
 package mp7.starwarswiki;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity3();
+            }
+        });
+
+        }
+
+        public void openActivity2() {
+            Intent intent = new Intent(this, Activity2.class);
+            startActivity(intent);
+        }
+
+        public void openActivity3() {
+            Intent intent2 = new Intent(this, Activity3.class);
+            startActivity(intent2);
+        }
+
     }
-}
